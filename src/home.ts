fee05baa4d330d1164041ef920cc0c64a3fdc5dd
Yosua Nicolaus <cryodawn257@gmail.com>
2022-03-14 20:59:48 +0700

    add restaurant image, style home page

--- src/home.ts
+++ src/home.ts
@@ -1,12 +1,19 @@
 const home = document.createElement("main");
+home.id = "home";
+
+const welcome = document.createElement("div");
 
 const title = document.createElement("h1");
 title.textContent = "Fast Restaurant";
-
 const description = document.createElement("em");
 description.textContent = "fast food, high quality";
 
-home.appendChild(title);
-home.appendChild(description);
+welcome.appendChild(title);
+welcome.appendChild(description);
+
+const restaurant = document.createElement("section");
+
+home.appendChild(welcome);
+home.appendChild(restaurant);
 
 export default home;
